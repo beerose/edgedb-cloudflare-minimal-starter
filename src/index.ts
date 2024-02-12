4176f4a18e6e9dfee4cbed5eeb4f237f1ab9fd3b
beerose <alexsandra.sikora@gmail.com>
2024-02-12 15:57:26 +0100

    EdgeDB Setup

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,21 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
+import * as edgedb from 'edgedb';
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(_request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const client = edgedb.createHttpClient({
+			instanceName: env.EDGEDB_INSTANCE,
+			secretKey: env.EDGEDB_SECRET_KEY,
+		});
+		const movies = await client.query(`
+			select Movie {
+				title
+			}
+		  `);
+
+		return new Response(JSON.stringify(movies, null, 2), {
+			headers: {
+				'content-type': 'application/json;charset=UTF-8',
+			},
+		});
 	},
-};
+} satisfies ExportedHandler<Env>;
